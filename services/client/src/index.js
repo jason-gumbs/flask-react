270d630eb49ae4af2e5e050553d6f5cbe8489450
wxg8ihv <wilfred_gumbs@homedepot.com>
2019-08-19 15:41:28 -0400

    test

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';  // new
 import UsersList from './components/UsersList';
-
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
@@ -11,8 +11,14 @@ class App extends Component {
         super();
         // new
         this.state = {
-            users: []
+            users: [],
+            username: 'justatest',
+            email: '',
         };
+        this.addUser = this.addUser.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+
+
     };
 
     // new
@@ -30,22 +36,61 @@ class App extends Component {
             });
     };
 
+    addUser(event) {
+        event.preventDefault();
+        // new
+        const data = {
+            username: this.state.username,
+            email: this.state.email
+        };
+        // new
+        axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    handleChange(event) {
+        const obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj);
+    };
+
     render() {
-  return (
-    <section className="section">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-one-third">
-            <br/>
-            <h1 className="title is-1">All Users</h1>
-            <hr/><br/>
-            <UsersList users={this.state.users}/>
-          </div>
-        </div>
-      </div>
-    </section>
-  )
-}
+        return (
+            < section
+        className = "section" >
+            < div
+        className = "container" >
+            < div
+        className = "columns" >
+            < div
+        className = "column is-half" >
+            < br / >
+            < h1
+        className = "title is-1" > All
+        Users < /h1>
+        < hr / > < br / >
+        < AddUser
+        username = {this.state.username}
+        email = {this.state.email}
+        addUser = {this.addUser}
+        handleChange = {this.handleChange}
+
+        />
+        < br / > < br / >
+        < UsersList
+        users = {this.state.users}
+        />
+        < /div>
+        < /div>
+        < /div>
+        < /section>
+    )
+    }
 };
 
 ReactDOM.render(
