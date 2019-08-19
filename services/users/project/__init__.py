174a1e08dfc38403ce9755b95aa1d4579887206a
wxg8ihv <wilfred_gumbs@homedepot.com>
2019-08-19 11:38:35 -0400

    react front end

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,14 +1,17 @@
 import os
 
-from flask import Flask  # new
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS  # new
 
 
-# instantiate the db
+# instantiate the extensions
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+cors = CORS()  # new
 
 
-# new
 def create_app(script_info=None):
 
     # instantiate the app
@@ -20,6 +23,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
+    cors.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
